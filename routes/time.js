c90bfe6eef5fe8c3697e1cfb92bebff7337f824a
startPendulum <1730706674@qq.com>
2023-01-31 19:50:00 +0800

    更换文件目录

--- routes/time.js
+++ routes/time.js
@@ -7,7 +7,7 @@ router.post('/', function (req, res, next) {
 	let {ip,location,time}=req.body
 	console.log(req.body)
 	let data=`${i} ip：${ip}，位置：${location}，时间：${time}\n`.toString()
-	fs.writeFile(path.join(process.cwd(),"./visitorInfo.txt"),data,{flag:"a"},(err)=>{
+	fs.writeFile(path.join(process.cwd(),"./visitorInfo/visitorInfo.txt"),data,{flag:"a"},(err)=>{
 		if(err){
 			console.log(err)
 		}
